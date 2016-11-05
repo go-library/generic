f51687dd87cdc30d2bfad50df44bea756370937b
Jeehoon <wlgns823@gmail.com>
2016-11-05 07:31:21 +0000

    functions that return reflect.Value are unexported

--- cursor.go
+++ cursor.go
@@ -32,10 +32,10 @@ func (c *Cursor) Index(keys ...interface{}) (nextCursor *Cursor) {
 	for i := range keys {
 		k := reflect.ValueOf(keys[i])
 
-		nextCursor.prepareToNext(nextCursor.Value(), k, false, false)
-		nextCursor.parent = nextCursor.Value()
+		nextCursor.prepareToNext(nextCursor.value(), k, false, false)
+		nextCursor.parent = nextCursor.value()
 		nextCursor.myKey = k
-		if !nextCursor.Value().IsValid() {
+		if !nextCursor.value().IsValid() {
 			panic(Errorf("key is undefined: %v", k))
 		}
 	}
@@ -51,10 +51,10 @@ func (c *Cursor) SetIndex(keys ...interface{}) (nextCursor *Cursor) {
 	for i := range keys {
 		k := reflect.ValueOf(keys[i])
 
-		nextCursor.prepareToNext(nextCursor.Value(), k, true, true)
-		nextCursor.parent = nextCursor.Value()
+		nextCursor.prepareToNext(nextCursor.value(), k, true, true)
+		nextCursor.parent = nextCursor.value()
 		nextCursor.myKey = k
-		if !nextCursor.Value().IsValid() {
+		if !nextCursor.value().IsValid() {
 			nextCursor.setEmpty()
 		}
 	}
@@ -65,18 +65,18 @@ func (c *Cursor) SetIndex(keys ...interface{}) (nextCursor *Cursor) {
 /* GETTERS */
 
 func (c *Cursor) Interface() interface{} {
-	if c.Value().IsValid() {
-		return c.Value().Interface()
+	if c.value().IsValid() {
+		return c.value().Interface()
 	} else {
 		return nil
 	}
 }
 
 func (c *Cursor) String() string {
-	return c.Value().String()
+	return c.value().String()
 }
 
-func (c *Cursor) Value() reflect.Value {
+func (c *Cursor) value() reflect.Value {
 	var value reflect.Value
 
 	switch c.parent.Kind() {
@@ -100,56 +100,56 @@ func (c *Cursor) Value() reflect.Value {
 /* SLICE FUNCTIONS */
 
 func (c *Cursor) Len() int {
-	if c.Value().Kind() != reflect.Slice {
+	if c.value().Kind() != reflect.Slice {
 		panic(Errorf("this value is not slice"))
 	}
 
-	return c.Value().Len()
+	return c.value().Len()
 }
 
 func (c *Cursor) Push(values ...interface{}) {
 	var vars []reflect.Value
 	for i := range values {
 		if values[i] == nil {
-			vars = append(vars, reflect.Zero(c.Value().Type().Elem()))
+			vars = append(vars, reflect.Zero(c.value().Type().Elem()))
 		} else {
 			vars = append(vars, reflect.ValueOf(values[i]))
 		}
 	}
 
-	c.PushValues(vars...)
+	c.pushValues(vars...)
 }
 
-func (c *Cursor) PushValues(values ...reflect.Value) {
-	if c.Value().Kind() != reflect.Slice {
+func (c *Cursor) pushValues(values ...reflect.Value) {
+	if c.value().Kind() != reflect.Slice {
 		panic(Errorf("this value is not slice"))
 	}
 
-	c.SetValue(reflect.Append(c.Value(), values...))
+	c.setValue(reflect.Append(c.value(), values...))
 }
 
 func (c *Cursor) Slice(i, j int) (nextCursor *Cursor) {
-	v := c.Value().Slice(i, j)
+	v := c.value().Slice(i, j)
 	nc := NewCursor(new(interface{}))
-	nc.SetValue(v)
+	nc.setValue(v)
 	return nc
 }
 
 func (c *Cursor) Slice3(i, j, k int) (nextCursor *Cursor) {
-	v := c.Value().Slice3(i, j, k)
+	v := c.value().Slice3(i, j, k)
 	nc := NewCursor(new(interface{}))
-	nc.SetValue(v)
+	nc.setValue(v)
 	return nc
 }
 
 /* MAP FUNCTIONS */
 
 func (c *Cursor) Keys() (keys []string) {
-	if c.Value().Kind() != reflect.Map {
+	if c.value().Kind() != reflect.Map {
 		panic(Errorf("this value is not map"))
 	}
 
-	keyValues := c.Value().MapKeys()
+	keyValues := c.value().MapKeys()
 	for i := range keyValues {
 		keys = append(keys, keyValues[i].String())
 	}
@@ -165,11 +165,19 @@ func (c *Cursor) Set(value interface{}) {
 		return
 	}
 
-	c.SetValue(reflect.ValueOf(value))
+	c.setValue(reflect.ValueOf(value))
 }
 
-/* Set value, low level controllor */
-func (c *Cursor) SetValue(value reflect.Value) {
+func (c *Cursor) Delete() {
+	switch c.parent.Kind() {
+	case reflect.Map:
+		c.setValue(reflect.Value{})
+	default:
+		c.setEmpty()
+	}
+}
+
+func (c *Cursor) setValue(value reflect.Value) {
 	switch c.parent.Kind() {
 	case reflect.Interface:
 		c.parent.Set(value)
@@ -182,29 +190,20 @@ func (c *Cursor) SetValue(value reflect.Value) {
 	}
 }
 
-func (c *Cursor) Delete() {
-	switch c.parent.Kind() {
-	case reflect.Map:
-		c.SetValue(reflect.Value{})
-	default:
-		c.setEmpty()
-	}
-}
-
 func (c *Cursor) setMap() {
-	c.SetValue(makeMap())
+	c.setValue(makeMap())
 }
 
 func (c *Cursor) setSlice() {
-	c.SetValue(makeSlice(0, 0))
+	c.setValue(makeSlice(0, 0))
 }
 
 func (c *Cursor) setEmpty() {
 	if c.parent.IsValid() {
 		if c.parent.Kind() == reflect.Interface {
-			c.SetValue(reflect.Zero(c.parent.Type()))
+			c.setValue(reflect.Zero(c.parent.Type()))
 		} else {
-			c.SetValue(reflect.Zero(c.parent.Type().Elem()))
+			c.setValue(reflect.Zero(c.parent.Type().Elem()))
 		}
 	} else {
 		panic(Errorf("parent value is invalid"))
@@ -280,7 +279,7 @@ func (c *Cursor) prepareToNext(value, key reflect.Value, permitCreate bool, perm
 	}
 
 	if isCreated {
-		c.SetValue(nextValue)
+		c.setValue(nextValue)
 	}
 }
 
